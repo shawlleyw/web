29723c3e6090e12dab9ea8248be4dd9822714435
wsy <540253314@qq.com>
2021-01-30 16:45:44 +0800

    fifth

--- static/src.js
+++ static/src.js
@@ -1,43 +1,48 @@
-function previewImage(fileDom){
+function previewImage(fileDom) {
 	var reader = new FileReader();
 	var file = fileDom.files[0];
 	var imageType = /^image\//;
-	if(!imageType.test(file.type)) {
+	if (!imageType.test(file.type)) {
 		alert("select image");
 		return;
 	}
-	reader.onload = function(element) {
+	reader.onload = function (element) {
 		var img = document.getElementById("preview");
 		img.src = element.target.result;
 		img.removeAttribute("hidden");
-		
 	}
 	reader.readAsDataURL(file);
 }
-function sendImage(){
+function sendImage() {
 	document.getElementById('waiting').innerHTML = 'waiting for host...'
-	var formData = new FormData(); 
-	formData.append('file', $('#file')[0].files[0]);  
-	formData.append('sizeid',123);  
-	var send_xhttp = new XMLHttpRequest();
-	send_xhttp.onreadystatechange = function(){
-		if(this.readyState == 4 && this.status == 200){
+	var formData = new FormData();
+	formData.append('photo', $('#file')[0].files[0]);
+	$.ajax({
+		type: "post",
+		url: "http://cat.wakk.top/run",
+		data: formData,
+		dataType: 'json',
+		processData: false, 
+		contentType: false,
+		xhrFields: { withCredentials: true },
+		async: true,    
+		success: function (data) {
 			getInfo();
-		}
-	}
-	send_xhttp.open("POST", "cat.wakk.top/run", true);
-	send_xhttp.send(formData);
+		},
+		error: function (data) {
+		},
+	});
 }
-function getInfo(){
-	var receive_xhttp = new XMLHttpRequest();
-	receive_xhttp.onreadystatechange = function(){
-		if(this.readyState == 4 && this.status == 200){
-			document.getElementById('waiting').innerHTML='';
-			writePage($.parseJSON(receive_xhttp.responseText))
-		}
-	}
-	receive_xhttp.open("GET", "http://cat.wakk.top/run/", true)
-	receive_xhttp.send()
+function getInfo() {
+	$.ajax({
+		type: "get",
+		url: "http://cat.wakk.top/run",
+		dataType: 'json',   
+		success: function (result) {
+		},
+		error: function (result) {
+		},
+	});
 }
 function writePage(text) {
 	document.getElementById('catinfo').innerHTML = text;
